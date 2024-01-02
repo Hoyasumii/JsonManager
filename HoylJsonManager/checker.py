8edadf4a4319cddc5f2897ea1fce8798d1419ed3
Alan Reis <alanreisanjo@gmail.com>
2024-01-02 19:42:13 -0300

    Melhorias no verificador

--- HoylJsonManager/checker.py
+++ HoylJsonManager/checker.py
@@ -1,9 +1,24 @@
-import json
+import json, os
 
 def checker(jsonFile, jsonSchema):
 
-    assert type(jsonFile) == str, "jsonFile must be a string"
-    assert type(jsonSchema) == dict, "jsonSchema must be a dictionary"
+    assert type(jsonFile) == str, "jsonFile must be a string."
+    assert os.path.isfile(jsonFile), "jsonFile must be a file."
+
+    if (type(jsonSchema) == str):
+
+        assert os.path.isfile(jsonSchema), "jsonSchema must be a file."
+
+        with open(jsonSchema) as json_file:
+            jsonSchema = json.load(json_file)
+
+            for item in jsonSchema.values():
+                assert item in [ "int", "float", "bool", "str", "list", "dict" ], "jsonSchema must be a valid schema."
+
+            jsonSchema = { key: eval(value) for key, value in jsonSchema.items() }
+            
+    else:
+        assert type(jsonSchema) == dict, "jsonSchema must be a dictionary."
 
     jsonFile += ".json" if jsonFile.endswith(".json") == False else ""
 
