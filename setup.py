bcdab6613f939eec84c28a0098ff1770bac8cead
Alan Reis <alanreisanjo@gmail.com>
2024-01-02 02:20:40 -0300

    Correção no Setup

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 with open("./README.md", "r") as arq:
     readme = arq.read()
 
-setup(name='JSON Checker',
+setup(name='JSON Manager',
     version='1.0',
     license='MIT License',
     author='Alan Reis Anjos',
     long_description=readme,
     long_description_content_type="text/markdown",
     author_email='alanreisanjo@gmail.com',
-    keywords='JSON Checker',
-    description=u'Pequena biblioteca para verificar se um arquivo JSON está de acordo com um schema',
-    packages=['jsonChecker'])
+    keywords='JSON Manager',
+    description=u'Pequena biblioteca para validação e manipulação de JSONs;',
+    packages=['jsonManager'])
